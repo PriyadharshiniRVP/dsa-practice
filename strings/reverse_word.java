753c30894d0983f8cb44c5caa1aa76fa1950ff9e
PriyadharshiniR <priyarama2209@gmail.com>
2026-03-08 00:25:21 +0530

    update reverse_word

--- strings/reverse_word.java
+++ strings/reverse_word.java
@@ -13,15 +13,15 @@ public class reverse_word {
     System.out.println("Reversed Sentence: "+ reverse(sentence));
     }
     public static String reverse (String s){
-        String trimmed=s.trim();
+        String trimmed=s.trim();  // removes the spaces 
         if(trimmed.isEmpty()){
             System.out.println("Reversed Sentence is:");
             return null ;
          }
-         String[] words=trimmed.split("\\s+");
+         String[] words=trimmed.split("\\s+"); // spliting it 
          StringBuilder reversedSentence=new StringBuilder();
          for(int i=words.length-1;i>=0;i--){
-            reversedSentence.append(words[i]);
+            reversedSentence.append(words[i]); //appending it
 
             if(i!=0){
                 reversedSentence.append("");
